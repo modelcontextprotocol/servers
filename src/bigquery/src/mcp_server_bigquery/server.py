b035ef0be9d53ab4d96b39537dbee7391d674aad
Lucas Hild <lucas.hild@knowunity.com>
2024-11-30 12:00:55 +0100

    Bump version to 0.2.0

--- src/bigquery/src/mcp_server_bigquery/server.py
+++ src/bigquery/src/mcp_server_bigquery/server.py
@@ -175,7 +175,7 @@ async def main(project: str, location: str, datasets_filter: list[str]):
             write_stream,
             InitializationOptions(
                 server_name="bigquery",
-                server_version="0.1.0",
+                server_version="0.2.0",
                 capabilities=server.get_capabilities(
                     notification_options=NotificationOptions(),
                     experimental_capabilities={},
