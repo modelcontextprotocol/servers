cc9d6cf2ef5b57c6a46fbb61db85a793b9067511
Mike Casazza <mfcasazza@gmail.com>
2025-03-01 09:48:28 -0500

    adding test for gh pr comment

--- src/filesystem/__tests__/path-utils.test.ts
+++ src/filesystem/__tests__/path-utils.test.ts
@@ -98,6 +98,32 @@ describe('Path Utilities', () => {
       expect(normalizePath('C:/Program Files/App Name'))
         .toBe('C:\\Program Files\\App Name');
     });
+
+    it('handles special characters in paths', () => {
+      // Test ampersand in path
+      expect(normalizePath('C:\\NS\\Sub&Folder'))
+        .toBe('C:\\NS\\Sub&Folder');
+      expect(normalizePath('C:/NS/Sub&Folder'))
+        .toBe('C:\\NS\\Sub&Folder');
+      expect(normalizePath('/mnt/c/NS/Sub&Folder'))
+        .toBe('C:\\NS\\Sub&Folder');
+      
+      // Test tilde in path (short names in Windows)
+      expect(normalizePath('C:\\NS\\MYKIND~1'))
+        .toBe('C:\\NS\\MYKIND~1');
+      expect(normalizePath('/Users/NEMANS~1/FOLDER~2/SUBFO~1/Public/P12PST~1'))
+        .toBe('/Users/NEMANS~1/FOLDER~2/SUBFO~1/Public/P12PST~1');
+      
+      // Test other special characters
+      expect(normalizePath('C:\\Path with #hash'))
+        .toBe('C:\\Path with #hash');
+      expect(normalizePath('C:\\Path with (parentheses)'))
+        .toBe('C:\\Path with (parentheses)');
+      expect(normalizePath('C:\\Path with [brackets]'))
+        .toBe('C:\\Path with [brackets]');
+      expect(normalizePath('C:\\Path with @at+plus$dollar%percent'))
+        .toBe('C:\\Path with @at+plus$dollar%percent');
+    });
   });
 
   describe('expandHome', () => {
