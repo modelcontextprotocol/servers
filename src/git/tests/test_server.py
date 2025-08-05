7e440ec93fde4c139512455bb8fe12037ba22d97
Taj Baba <107571990+tajbaba999@users.noreply.github.com>
2025-08-05 22:05:33 +0530

    Update test_server.py

--- src/git/tests/test_server.py
+++ src/git/tests/test_server.py
@@ -90,4 +90,4 @@ def test_git_add_specific_files(test_repository):
     staged_files = [item.a_path for item in test_repository.index.diff("HEAD")]
     assert "file1.txt" in staged_files
     assert "file2.txt" in staged_files
-    assert result == "Files staged sucessfully"
+    assert result == "Files staged successfully"
